b251b6cec29c18b98bdd337ea2ce3ec6280765d2
LWANG267 <LWANG267@ford.com>
2022-04-25 20:39:44 +0800

    vite环境变量修改

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,7 +6,12 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'home',
     component: () => import('../views/Home/index.vue')
-  }
+  },
+  {
+    path: '/',
+    name: 'home1',
+    component: () => import('../views/Home/index.vue')
+  },
 ];
 
 export default routes;
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+const VITE_APP_TITLE = import.meta.env.VITE_APP_TITLE;
+console.log('VITE_APP_TITLE:' + VITE_APP_TITLE);
 </script>
 <template>
   <div>123</div>
